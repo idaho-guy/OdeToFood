2086170a5d38472f74941ac9f06b43b2892b1bc2
Mark Sage <idaho.coder@gmail.com>
2020-02-20 13:45:24 -0600

    give model access to config file

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private IConfiguration config;
+
         public string Message { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public void OnGet()
         {
-            Message = "hello";
+            Message = config["Message"];
         }
     }
 }
