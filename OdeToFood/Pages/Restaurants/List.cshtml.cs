9076c7ab30b873f6a5c2f3beebafceaad9a05d67
Mark Sage <idaho.coder@gmail.com>
2020-02-20 13:39:42 -0600

    make model attributes available to page

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -9,8 +9,10 @@ namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        public string Message { get; set; }
         public void OnGet()
         {
+            Message = "hello";
         }
     }
 }
