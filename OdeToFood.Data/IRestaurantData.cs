c8bb00d550944f3f61119192840859b45ced2d22
Mark Sage <idaho.coder@gmail.com>
2020-02-20 16:03:20 -0600

    get details or error page

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -8,6 +8,7 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -26,6 +27,12 @@ namespace OdeToFood.Data
             };
 
         }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return from r in restaurants
--- OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -5,21 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
         public Restaurant Restaurant { get; set; }
+        public IRestaurantData RestaurantData { get; }
 
-        public DetailModel()
+        public DetailModel(IRestaurantData restaurantData)
         {
+            RestaurantData = restaurantData;
         }
 
-        public void OnGet(int restaurantId)
+        public IActionResult OnGet(int restaurantId)
         {
-            Restaurant = new Restaurant();
-            Restaurant.Id = restaurantId;
+            Restaurant = RestaurantData.GetById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
         }
     }
 }
