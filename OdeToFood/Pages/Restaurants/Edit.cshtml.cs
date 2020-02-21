646d3a2188f5ef6ba5e7fcc4ae903f1d5a0998e3
Mark Sage <idaho.coder@gmail.com>
2020-02-21 09:11:08 -0600

    Adding create new

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -26,10 +26,20 @@ namespace OdeToFood.Pages.Restaurants
 
         
 
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
             Cusines = htmlHelper.GetEnumSelectList<CuisineType>();
-            Restaurant = restaurantData.GetById(restaurantId);
+            if(restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+
+            }
+
+            
             if(Restaurant == null)
             {
                 return RedirectToPage("./NotFound");
