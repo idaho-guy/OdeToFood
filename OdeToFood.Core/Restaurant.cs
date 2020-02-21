77bfb1692b68ee40f78a81126e458b707b325eb2
Mark Sage <idaho.coder@gmail.com>
2020-02-21 08:53:06 -0600

    Error messaging

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace OdeToFood.Core
 {
     public class Restaurant
     {
+        [Required, StringLength(80)]
         public string Name { get; set;}
+        [Required, StringLength(255)]
         public string Location { get; set; }
         public int Id { get; set; }
         public CuisineType Cusine { get; set;}
--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -39,9 +39,13 @@ namespace OdeToFood.Pages.Restaurants
 
         public IActionResult OnPost()
         {
+            if(ModelState.IsValid)
+            {
+                restaurantData.Update(Restaurant);
+                restaurantData.Commit();
+            }
             Cusines = htmlHelper.GetEnumSelectList<CuisineType>();
-            restaurantData.Update(Restaurant);
-            restaurantData.Commit();
+            
             return Page();
         }
     }
